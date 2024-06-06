173deb973f089661bbcd100f031bc0d72e821232
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-06-06 20:10:17 +0700

    Update Guard di Role Seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,7 +14,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create(['name' => 'admin']);
-        Role::create(['name' => 'company']);
+        Role::create(['name' =>'company', 'guard_name' => 'company']);
         Role::create(['name' => 'user']);
     }
 }
