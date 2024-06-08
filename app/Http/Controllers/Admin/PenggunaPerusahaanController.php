1a0a8d9eaacc521e42a86a05ff1eb52abbcd3bef
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-06-08 11:13:29 +0700

    Implementasi Create Akun Perusahaan di Admin
    - Menambahkan route ke action form
    - Menambahkan dirrective error untuk input form yang required
    - Membuat request untuk validasi data input
    - Menambahkan logic pada method createproses() di controller

--- app/Http/Controllers/Admin/PenggunaPerusahaanController.php
+++ app/Http/Controllers/Admin/PenggunaPerusahaanController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePerusahaanRequest;
+use App\Http\Requests\RegisterRequest;
 
 class PenggunaPerusahaanController extends Controller
 {
@@ -18,8 +20,11 @@ class PenggunaPerusahaanController extends Controller
         return view('admin.pengguna.perusahaan.create');
     }
 
-    public function createproses() {
+    public function createproses(CreatePerusahaanRequest $request) {
+        $company = Company::create($request->validated());
+        $company->assignRole('company');
 
+        return redirect()->route('admin.pengguna.index')->with('success', 'Perusahaan berhasil ditambahkan');
     }
 
     public function edit() {
--- resources/views/admin/pengguna/perusahaan/create.blade.php
+++ resources/views/admin/pengguna/perusahaan/create.blade.php
@@ -9,118 +9,132 @@
 @endsection
 
 @section('content')
-
-<div id="app">
-    <div class="main-wrapper main-wrapper-1">
-        <div class="main-content">
-            <section class="section">
-              <div class="section-header">
-                <h1>Pengguna Mitra Perusahaan</h1>
-                @include('admin.pengguna.perusahaan.navbarperusahaan')
-              </div>
-
-              <div class="section-body">
-                <div class="row">
-                    <div class="col-12 col-md-8 col-lg-12">
-                        <div class="card">
-                            <form action="" method="POST">
-                              @csrf
-                              <div class="card-header">
-                                <h4>Form Create Penggunna Perusahaan</h4>
-                              </div>
-                              <div class="card-body">
-                                <div class="row">
-                                  <div class="col-md-6">
-                                    <div class="form-group">
-                                      <label>Company Name</label>
-                                      <input type="text" name="name" class="form-control" required>
-                                    </div>
-                                    <div class="form-group">
-                                      <label>PIC Name</label>
-                                      <input type="text" name="pic_name" class="form-control" required>
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Email</label>
-                                      <input type="email" name="email" class="form-control" required>
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Phone</label>
-                                      <input type="text" name="phone" class="form-control" required>
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Password</label>
-                                      <input type="password" name="password" class="form-control" required>
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Address</label>
-                                      <input type="text" name="address" id="address" class="form-control">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                    <div class="form-group">
-                                      <label>Profile Photo</label>
-                                      <input type="file" name="foto_profil" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Number of Employees</label>
-                                      <input type="number" name="employees" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Industry</label>
-                                      <input type="text" name="industry" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Website</label>
-                                      <input type="text" name="website" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Instagram</label>
-                                      <input type="text" name="instagram" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Facebook</label>
-                                      <input type="text" name="facebook" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                      <label>Status</label>
-                                      <select name="status" class="form-control" required>
-                                        <option value="active">Active</option>
-                                        <option value="inactive">Inactive</option>
-                                      </select>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="card-footer text-right">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                              </div>
-                            </form>
-                          </div>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="main-content">
+                <section class="section">
+                    <div class="section-header">
+                        <h1>Pengguna Mitra Perusahaan</h1>
+                        @include('admin.pengguna.perusahaan.navbarperusahaan')
                     </div>
-                </div> 
-            
-              </div>
 
-            </section>
+                    <div class="section-body">
+                        <div class="row">
+                            <div class="col-12 col-md-8 col-lg-12">
+                                <div class="card">
+                                    <form action="{{ route('admin.pengguna.createproses') }}" method="POST">
+                                        @csrf
+                                        <div class="card-header">
+                                            <h4>Form Create Penggunna Perusahaan</h4>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label>Company Name</label>
+                                                        <input type="text" name="name" class="form-control" required>
+                                                        @error('name')
+                                                            <div class="text-danger mt-2">{{ $message }}</div>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>PIC Name</label>
+                                                        <input type="text" name="pic_name" class="form-control" required>
+                                                        @error('pic_name')
+                                                            <div class="text-danger mt-2">{{ $message }}</div>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Email</label>
+                                                        <input type="email" name="email" class="form-control" required>
+                                                        @error('email')
+                                                            <div class="text-danger mt-2">{{ $message }}</div>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Phone</label>
+                                                        <input type="text" name="phone" class="form-control" required>
+                                                        @error('phone')
+                                                            <div class="text-danger mt-2">{{ $message }}</div>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Password</label>
+                                                        <input type="password" name="password" class="form-control"
+                                                            required>
+                                                        @error('password')
+                                                            <div class="text-danger mt-2">{{ $message }}</div>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Address</label>
+                                                        <input type="text" name="address" id="address"
+                                                            class="form-control">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label>Profile Photo</label>
+                                                        <input type="file" name="foto_profil" class="form-control">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Number of Employees</label>
+                                                        <input type="number" name="employees" class="form-control">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Industry</label>
+                                                        <input type="text" name="industry" class="form-control">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Website</label>
+                                                        <input type="text" name="website" class="form-control">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Instagram</label>
+                                                        <input type="text" name="instagram" class="form-control">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Facebook</label>
+                                                        <input type="text" name="facebook" class="form-control">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Status</label>
+                                                        <select name="status" class="form-control" required>
+                                                            <option value="active">Active</option>
+                                                            <option value="inactive">Inactive</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer text-right">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </section>
+            </div>
         </div>
     </div>
-</div> 
-
 @endsection
 
 
 {{-- CKEDITOR --}}
 @section('scripts')
+    <script>
+        $(document).ready(function() {
+            // Inisialisasi CKEditor pada modal tambah data
+            ClassicEditor
+                .create(document.querySelector('#address'))
+                .catch(error => {
+                    console.error(error);
+                });
 
-<script>
-$(document).ready(function() {
-    // Inisialisasi CKEditor pada modal tambah data
-    ClassicEditor
-        .create(document.querySelector('#address'))
-        .catch(error => {
-            console.error(error);
         });
-
-});
-</script>
+    </script>
 @endsection
