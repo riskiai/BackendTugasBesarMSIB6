0bae4d89494d9954dd28a32771efeba02fe5c25c
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-06-10 16:08:43 +0700

    Membuat dan Menghapus Migration dan Model
    - Membuat migration lowongans dan apply_lowongans
    - Menghapus migration social_network dan bantuan
    - Membuat model Lowongan dan ApplyLowongan
    - Menghapus model SocialNetwork dan Bantuan

--- database/migrations/2024_06_10_040357_create_apply_lowongans_table.php
+++ database/migrations/2024_06_10_040357_create_apply_lowongans_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('social_networks', function (Blueprint $table) {
+        Schema::create('apply_lowongans', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('lowongan_id');
             $table->unsignedBigInteger('user_id');
-            $table->string('instagram')->nullable();
-            $table->string('twitter')->nullable();
-            $table->string('facebook')->nullable();
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('social_networks');
+        Schema::dropIfExists('apply_lowongans');
     }
 };
