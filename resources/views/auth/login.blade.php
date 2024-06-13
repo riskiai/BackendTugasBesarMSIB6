5de399b0f5deafdc7575d45cc00f739dd93e36e2
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-06-13 13:27:25 +0700

    UI: Mengimplementasikan Tampilan Baru Halaman Login
    - Menambahkan background image pada halaman login
    - Melakukan perubahan warna dan bentuk komponen, serta font
    
    - Menambahkan file app.css ke layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-7 mx-auto" style="margin-left: 30px">
-                <div class="card shadow-sm p-3 border-0">
-                    <div class="row">
-                        {{-- error message --}}
-                        @if (session('error'))
-                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                {{ session('error') }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div>
-                        @endif
+    <div class="w-100 h-100"
+        style="position: fixed; no-repeat center center; background-size: cover; background-image: url('images/login-background.svg')">
+        <div class="w-100 h-100 pb-5 d-flex flex-column justify-content-center align-items-center"
+            style="background-color: rgba(30, 30, 30, 0.7)">
+            <h2 class="text-white mb-4">APPRENTICE AND TECHNOLOGY</h2>
 
-                        <div class="col-md-5">
-                            <img src="{{ asset('assets/img/Man.png') }}" width="250" height="267" alt=""
-                                style="margin-top: 100px" />
-                        </div>
-
-                        <div class="col-md-7">
-                            <div class="card-body">
-                                <h4>Login</h4>
-                                <form action="{{ route('login.authenticate') }}" method="POST">
-                                    @csrf
-                                    <div class="mb-3">
-                                        <label for="email" class="form-label">Email</label>
-                                        <input type="email" name="email" class="form-control" id="email"
-                                            placeholder="masukkan email kamu" />
-                                        @error('email')
-                                            <div class="text-danger mt-2">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-
-                                    <div class="mb-3">
-                                        <label for="password" class="form-label">Password</label>
-                                        <input type="password" name="password" class="form-control" id="password"
-                                            placeholder="masukkan password" />
-                                        @error('password')
-                                            <div class="text-danger mt-2">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                    <div style="text-align: center; margin-top: 20px">
-                                        <button type="submit" class="btn btn-custom">Submit</button>
-                                        <p style="margin-top: 5px; margin-bottom: 5px">
-                                            Sudah memiliki akun?
-                                        </p>
-                                        <a href="{{ url('register') }}" type="button" class="btn btn-custom btn-masuk">
-                                            Register
-                                        </a>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+            {{-- error message --}}
+            @if (session('error'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
+            @endif
+
+            <div class="card shadow-sm p-3 border-0 mb-4" style="min-width: 500px; background-color: #CEDDE6; border-radius: 20px">
+                <h2 class="text-center font-koulen font-semibold">Login</h2>
+                <form action="{{ route('login.authenticate') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" style="background-color: #EEF5FF; border-radius: 20px" id="email"
+                            placeholder="masukkan email kamu" />
+                        @error('email')
+                            <div class="text-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" style="background-color: #EEF5FF; border-radius: 20px" id="password"
+                            placeholder="masukkan password" />
+                        @error('password')
+                            <div class="text-danger mt-2">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div style="text-align: center; margin-top: 20px">
+                        <button type="submit" class="text-white font-koulen px-5 fs-5 mb-2" style="background-color: #074173; border-radius: 20px;">MASUK</button>
+                        <p style="margin-top: 5px; margin-bottom: 5px">
+                            Sudah memiliki akun?
+                        </p>
+                        <a href="{{ url('register') }}" type="button" class="btn btn-custom btn-masuk">
+                            Register
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,16 @@
     <link rel="stylesheet" href="{{ asset('css/tampilan-register.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/artikel_masuk.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/detail_webinar.css') }}" />
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    
     {{-- jquery --}}
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+
+    {{-- Font Koulen --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Koulen&display=swap" rel="stylesheet">
 </head>
 
 <body>
