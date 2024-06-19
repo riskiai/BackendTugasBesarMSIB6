d15278dfd8d341e6d08f08bc6df10b91fdcbbbf0
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-06-20 06:19:09 +0700

    UI: Menghapus dummy text pada detail artikel

--- resources/views/pages/detail-artikel.blade.php
+++ resources/views/pages/detail-artikel.blade.php
@@ -13,71 +13,8 @@
                             <h6>
                                 {{ $artikel->created_at->diffForHumans() }}
                             </h6>
-                            <p>
-                                {{ $artikel->konten}}
-                            </p>
+                            {!! $artikel->konten !!}
                         </div>
-
-                        <div>
-                            <h5>Tantangan:</h5>
-                            <ol>
-                                <li>
-                                    Tingginya Persaingan: Industri teknologi seringkali
-                                    dibanjiri oleh pelamar yang berkompeten dan berbakat.
-                                    Persaingan yang ketat dapat membuat sulit bagi magang untuk
-                                    memperoleh pengalaman yang diinginkan atau bahkan
-                                    mendapatkan posisi magang tersebut.
-                                </li>
-                                <li>
-                                    Kesenjangan Pengetahuan: Teknologi terus berkembang dengan
-                                    cepat, dan seringkali magang harus beradaptasi dengan
-                                    teknologi-teknologi baru yang mungkin belum dipelajari
-                                    sebelumnya.
-                                </li>
-                                <li>
-                                    Tuntutan Kinerja Tinggi: Industri teknologi dikenal dengan
-                                    ritme kerja yang cepat dan tuntutan kinerja yang tinggi.
-                                    Magang mungkin merasa tertekan untuk menyelesaikan
-                                    tugas-tugas dengan cepat dan akurat.
-                                </li>
-                                <li>
-                                    Ketergantungan pada Teknologi: Terkadang, terlalu bergantung
-                                    pada teknologi dapat mengurangi interaksi sosial antar rekan
-                                    kerja, yang mana dapat mengurangi kesempatan untuk membangun
-                                    hubungan dan jaringan profesional.
-                                </li>
-                            </ol>
-                        </div>
-                    </div>
-                    <div>
-                        <h5>tips sukses:</h5>
-                        <ol>
-                            <li>
-                                Tingginya Persaingan: Industri teknologi seringkali dibanjiri
-                                oleh pelamar yang berkompeten dan berbakat. Persaingan yang
-                                ketat dapat membuat sulit bagi magang untuk memperoleh
-                                pengalaman yang diinginkan atau bahkan mendapatkan posisi
-                                magang tersebut.
-                            </li>
-                            <li>
-                                Kesenjangan Pengetahuan: Teknologi terus berkembang dengan
-                                cepat, dan seringkali magang harus beradaptasi dengan
-                                teknologi-teknologi baru yang mungkin belum dipelajari
-                                sebelumnya.
-                            </li>
-                            <li>
-                                Tuntutan Kinerja Tinggi: Industri teknologi dikenal dengan
-                                ritme kerja yang cepat dan tuntutan kinerja yang tinggi.
-                                Magang mungkin merasa tertekan untuk menyelesaikan tugas-tugas
-                                dengan cepat dan akurat.
-                            </li>
-                            <li>
-                                Ketergantungan pada Teknologi: Terkadang, terlalu bergantung
-                                pada teknologi dapat mengurangi interaksi sosial antar rekan
-                                kerja, yang mana dapat mengurangi kesempatan untuk membangun
-                                hubungan dan jaringan profesional.
-                            </li>
-                        </ol>
                     </div>
                 </div>
 
