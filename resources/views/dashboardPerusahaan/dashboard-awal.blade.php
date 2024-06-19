99d1875df00de523eb7ae5d0594b5112b7f2cd12
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-06-20 06:19:56 +0700

    UI: Menengahkan ringkasan informasi pada dashboard awal perusahaan dan mengganti teks keterangan

--- resources/views/dashboardPerusahaan/dashboard-awal.blade.php
+++ resources/views/dashboardPerusahaan/dashboard-awal.blade.php
@@ -4,51 +4,53 @@
     <section>
         <div class="container text-center p-0" style="width: 100%;">
             <div class="row">
-                <!-- main start -->
-                <div class="col-9 mt-5">
-                    <div class="card w-100" style="background-color: #074173; padding: 10px; height: 100%;">
-                        <div class="container text-center p-0" style="width: 100%;">
-                            <div class="row ">
-                                <div class="col-md-6 mt-4">
-                                    <div class="card border-primary border-2">
-                                        <div class="card-body d-flex align-items-center justify-content-between">
-                                            <div class="d-flex align-items-center">
-                                                <i class="fas fa-briefcase fa-2x"></i>
-                                                <span class="ms-3"><i class="bi bi-briefcase"></i> Jumlah Post
-                                                    Lowongan</span>
+                <div class="d-flex w-100 justify-content-center">
+                    <!-- main start -->
+                    <div class="col-9 mt-5">
+                        <div class="card w-100" style="background-color: #074173; padding: 10px; height: 100%;">
+                            <div class="container text-center p-0" style="width: 100%;">
+                                <div class="row ">
+                                    <div class="col-md-6 mt-4">
+                                        <div class="card border-primary border-2">
+                                            <div class="card-body d-flex align-items-center justify-content-between">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="fas fa-briefcase fa-2x"></i>
+                                                    <span class="ms-3"><i class="bi bi-briefcase"></i> Jumlah Post
+                                                        Lowongan</span>
+                                                </div>
+                                                <span>{{ $lowonganCount }}</span>
                                             </div>
-                                            <span>{{ $lowonganCount }}</span>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-6 mt-4">
-                                    <div class="card border-primary">
-                                        <div class="card-body d-flex align-items-center justify-content-between">
-                                            <div class="d-flex align-items-center">
-                                                <i class="fas fa-users fa-2x"></i>
-                                                <span class="ms-3"><i class="bi bi-people"></i> Jumlah Pendaftar</span>
+                                    <div class="col-md-6 mt-4">
+                                        <div class="card border-primary">
+                                            <div class="card-body d-flex align-items-center justify-content-between">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="fas fa-users fa-2x"></i>
+                                                    <span class="ms-3"><i class="bi bi-people"></i> Jumlah Pendaftar Lowongan</span>
+                                                </div>
+                                                <span>{{ $applyCount }}</span>
                                             </div>
-                                            <span>{{ $applyCount }}</span>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-6 mt-4">
-                                    <div class="card border-primary">
-                                        <div class="card-body d-flex align-items-center justify-content-between">
-                                            <div class="d-flex align-items-center">
-                                                <i class="fas fa-chalkboard-teacher fa-2x"></i>
-                                                <span class="ms-3"><i class="bi bi-shop-window"></i> Jumlah yang Mengikuti
-                                                    Webinar</span>
+                                    <div class="col-md-6 mt-4">
+                                        <div class="card border-primary">
+                                            <div class="card-body d-flex align-items-center justify-content-between">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="fas fa-chalkboard-teacher fa-2x"></i>
+                                                    <span class="ms-3"><i class="bi bi-shop-window"></i> Jumlah Pendaftar
+                                                        Webinar</span>
+                                                </div>
+                                                <span>{{ $webinarAttendeeCount }}</span>
                                             </div>
-                                            <span>{{ $webinarAttendeeCount }}</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <!-- main end -->
                 </div>
-                <!-- main end -->
             </div>
         </div>
     </section>
